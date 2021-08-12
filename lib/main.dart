bed30dab9b9f66768526c8114ad38255b86b5af3
Daniel <deepblue.daniel@gmail.com>
2021-08-12 12:15:40 +0900

    chewie

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -59,9 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Text('Go to HTML player')),
               Text('W'),
 
-              /// https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/45d870155285890812491498931/24c2SGTVjrcA.mp4
-              /// https://vod4.buycar5.cn/20210512/mYvB1MkB/index.m3u8
-              /// https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8
+              // https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/45d870155285890812491498931/24c2SGTVjrcA.mp4
+              // https://vod4.buycar5.cn/20210512/mYvB1MkB/index.m3u8
+              // https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8
               MyVideoPlayer(key: Key('video $_playIndex'), url: 'https://vod4.buycar5.cn/20210512/mYvB1MkB/index.m3u8'),
               Text('Video is CC-BY-NC-SA 2.5 from Svenskt Teckenspråkslexikon'),
             ],
@@ -91,6 +92,7 @@ class MyVideoPlayer extends StatefulWidget {
 
 class _MyVideoPlayerState extends State<MyVideoPlayer> {
   late VideoPlayerController _controller;
+  late ChewieController _chewieController;
 
   @override
   void initState() {
@@ -102,6 +104,19 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
     _controller = VideoPlayerController.network(widget.url);
     await _controller.initialize();
     _controller.setVolume(0);
+
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      errorBuilder: (context, st) {
+        print('chewie controller load error: $st');
+        return Icon(
+          Icons.mood,
+          size: 64,
+          color: Colors.white,
+        );
+      },
+    );
+
     setState(() {
       _controller.play();
     });
@@ -110,6 +125,7 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
   @override
   void dispose() {
     _controller.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
@@ -118,7 +134,8 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
     return AspectRatio(
       aspectRatio: _controller.value.isInitialized ? _controller.value.aspectRatio : 16 / 9,
       child: _controller.value.isInitialized
-          ? VideoPlayer(_controller)
+          // ? VideoPlayer(_controller)
+          ? Chewie(controller: _chewieController)
           : Container(
               color: Colors.black,
               child: Center(
